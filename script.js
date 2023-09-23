ab75d352bd6c460e3f56821001a83761c1b3d889
Vikas Kumar <projectsbyvikas0@gmail.com>
2023-09-23 15:00:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body"),
+  hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second"),
+  modeSwitch = document.querySelector(".mode-switch");
+
+if (localStorage.getItem("mode") === "Red Theme") {
+  body.classList.add("dark");
+  modeSwitch.textContent = "Blue Theme";
+}
+
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  const isDarkMode = body.classList.contains("dark");
+  modeSwitch.textContent = isDarkMode ? "Blue Theme" : "Red Theme";
+  localStorage.setItem("mode", isDarkMode ? "Red Theme" : "Blue Theme");
+});
+
+const updateTime = () => {
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+
+setInterval(updateTime, 1000);
+
+updateTime();
